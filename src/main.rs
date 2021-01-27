88a1d8eac27ca9ec6c7ad411a2e23fcee7420ee3
Eric-exp <waterfall.strong@gmail.com>
2021-01-27 21:25:41 +1300

    Some simple starter code for twilight bot

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use twilight_command_parser::*;
+use twilight_model::*;
+
+let mut commands = CommandParserConfig::new();
+
+commands.add_prefix("mp/")
+
+fn main() {
+    println!("Hello, world!");
+}
