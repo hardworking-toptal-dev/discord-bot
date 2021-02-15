dfbfdf96e797e333076225a4546188ebc0465b32
duncy <fryerduncan@gmail.com>
2021-02-15 14:37:28 +1300

    updated the files

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,39 @@
-use twilight_command_parser::*;
-use twilight_model::*;
+use twilight_command_parser::{CommandParserConfig, Command, Parser}};
+use twilight_http::Client as Client;
+use twilight_gateway::{
+    cluster::{Cluster, ShardScheme},
+    Intents};
 
-let mut commands = CommandParserConfig::new();
+#[tokio::main]
+fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let token = env::var("TOKEN")?;
+    let shard = Shard::new(token, );
 
-commands.add_prefix("mp/")
+    let scheme = ShardScheme::try_from((0..1, 1))?;
+    let intents = Intents::GUILD_MESSAGES | Intents:: GUILDS;
 
-fn main() {
-    println!("Hello, world!");
+    let cluster = Cluster::builder(token, intents)
+        .shard_scheme(scheme)
+        .build()
+        .await?;
+    
+    let mut events = cluster.events();
+
+    let cluster_spawn = cluster.clone();
+
+    let mut commands = CommandParserConfig::new();
+    commands.add_prefix("mp/");
+    //commands.add_prefix(println("<@{}>", shard.id));
+
+    tokio::spawn(async move {
+        cluster_spawn.up().await;
+    });
+
+    while let Some(event) = events.next().await {
+        println!("EVENT: {:?}", event);
+    }
+
+    Ok(())
 }
+
+
